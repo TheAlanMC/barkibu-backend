ab2874bbc571a6716e9836fcd39bcc29070567d2
AndrePxd <andrepanique@gmail.com>
2022-11-14 00:41:22 -0400

    added Question Owner Dao

---
+++ src/main/java/bo/edu/ucb/barkibu/dao/QuestionOwnerDao.java
@@ -0,0 +1,26 @@
+package bo.edu.ucb.barkibu.dao;
+
+import bo.edu.ucb.barkibu.entity.QuestionOwner;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public interface QuestionOwnerDao {
+    @Select("""
+            SELECT pet.photo_path, problem as question, detailed_description, time_stamp
+            FROM question
+            JOIN category ON question.category_id = category.category_id
+            JOIN pet ON question.pet_id = pet.pet_id
+            JOIN "user" ON question.user_id = "user".user_id
+            WHERE "user".user_name = #{userName}
+            AND question.status = 'activo'
+            AND category.status = 'activo'
+            AND pet.status = 'activo'
+            AND "user".status = 'activo'
+            GROUP BY pet.photo_path, problem, detailed_description, question.time_stamp
+            ORDER BY question.time_stamp DESC;
+            """)
+    List<QuestionOwner> findOwnerQuestionByUserName(String userName);
+}
